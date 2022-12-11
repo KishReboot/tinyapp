d997590c239a27794cc7243f8bf57351c1d779e6
KishReboot <russellsalopek@pm.me>
2022-12-11 00:12:25 -0500

    Add functions from express_server.js to this file.

--- helpers.js
+++ helpers.js
@@ -11,6 +11,41 @@ const getUserByEmail = (email, database) => {
   }
 
   return undefined;
+
 };
 
-module.exports = { getUserByEmail };
+const userURLS = (id, database) => {
+
+  let userURLs = {};
+
+  for (const shortURL in database) {
+
+    if (database[shortURL].userID === id) {
+
+      userURLs[shortURL] = database[shortURL];
+
+    }
+
+  }
+
+  return userURLs;
+  
+};
+
+const generateRandomString = () => {
+
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let random = '';
+
+  for (let i = 0; i < 6; i++) {
+
+    random += chars[Math.floor(Math.random() * chars.length)];
+
+  }
+
+  return random;
+
+};
+
+
+module.exports = { getUserByEmail, generateRandomString, userURLS };
